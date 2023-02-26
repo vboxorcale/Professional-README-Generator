38baaaa642e31f1bc23e63b15a2d25136ddd4bcd
osboxes.org <milktealliance@protonmail.com>
2023-02-26 15:36:08 +0000

    update index.js and generateMarkdown.js

--- index.js
+++ index.js
@@ -5,6 +5,64 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type:'input',
+        name:'github',
+        message:'What is your GitHub username?🍄'
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'What is your Email address?📨'
+    },
+
+    {
+        type:'input',
+        name:'title',
+        message:"What is your project's name?"
+
+    },
+
+    {
+        type:'input',
+        name:'description',
+        message:'Please write a short description of your project?'
+    },
+
+    {
+        type:'list',
+        name:'license',
+        message:'What kind of license ?',
+        choices: ["MIT", "APACHE 2.0", "GPL", "None"]
+
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'What command should be run to install dependencies?',
+        default: 'npm i'
+    },
+
+    {
+        type:'input',
+        name:'test',
+        message:'What command should be run to testing?',
+        default: 'npm test'
+    },
+
+    {
+        type:'input',
+        name:'usage',
+        message:'What does the user need to know how about using the repo ?'
+        
+    },
+
+    {
+        type:'input',
+        name:'contributing',
+        message:'What does the user need to know about contributing to the repo?',
+        default: 'npm i'
+    },
 
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,19 @@
+
+function renderLicense(license) {
+  if (license !=="None"){
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return ``
+}
+
+
+
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicense(data.license)}
 
 `;
 }
